14b2fd0ca62484f47a2f8717e0808867ac482112
yunesss00 <48675571+yunesss00@users.noreply.github.com>
2023-06-04 17:51:12 +0200

    conectado flutter y firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'config/config.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   await Environment.initEnviroment();
+  await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ProviderScope(child: MainApp()));
 }
